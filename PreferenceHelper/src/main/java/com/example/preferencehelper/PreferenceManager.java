87ae2795f1050a173330c975b32e679d0bcb7638
LongSeong <gkaksh411@gmail.com>
2022-03-04 01:29:04 +0900

    PreferenceHelper initial commit

---
+++ PreferenceHelper/src/main/java/com/example/preferencehelper/PreferenceManager.java
@@ -0,0 +1,13 @@
+package com.example.preferencehelper;
+
+import java.util.LinkedList;
+
+public class PreferenceManager {
+
+    private final LinkedList<Preference> mPreferenceList;
+
+    public PreferenceManager() {
+        mPreferenceList = new LinkedList<>();
+    }
+
+}
